8b5a969c3825e896390472bab84fc3ff9c6039de
rcmdnk <rcmdnk@gmail.com>
2025-08-08 19:17:55 +0900

    chore: update pre-commit hooks and mise configuration refactor: lazily load __version__ using __getattr__

--- src/hydra_utils/__init__.py
+++ src/hydra_utils/__init__.py
@@ -1,4 +1,12 @@
-from .__version__ import __version__
 from .utils import hydra_wrapper, to_absolute_path
 
 __all__ = ['__version__', 'hydra_wrapper', 'to_absolute_path']
+
+
+def __getattr__(name: str) -> str:
+    if name == '__version__':
+        from .version import __version__
+
+        return __version__
+    msg = f'module {__name__} has no attribute {name}'
+    raise AttributeError(msg)
